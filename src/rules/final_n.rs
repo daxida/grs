05dceb726c56e01be2920f8cf6448fde1c4f5be1
daxida <galmaw@hotmail.com>
2025-02-06 13:37:15 +0100

    Clean `final_n.rs`

--- src/rules/final_n.rs
+++ src/rules/final_n.rs
@@ -1,90 +1,70 @@
-use crate::diagnostic::*;
+use crate::diagnostic::{Diagnostic, Fix};
 use crate::registry::Rule;
-use crate::tokenizer::*;
+use crate::tokenizer::{Doc, Token};
 use grac::is_vowel_el;
-use std::fmt::Write;
 
-fn remove_last_char(s: &str) -> String {
-    let sz = s.chars().collect::<Vec<_>>().len();
-    s.chars().take(sz - 1).collect()
-}
+#[rustfmt::skip]
+const PLOSIVE_CLUSTERS: [&str; 16] = [
+    "κ", "π", "τ", "μπ", "ντ", "γκ", "ξ", "ψ",
+    "Κ", "Π", "Τ", "Μπ", "Ντ", "Γκ", "Ξ", "Ψ"
+];
 
-const PLOSIVE_CLUSTERS: [&str; 8] = ["κ", "π", "τ", "μπ", "ντ", "γκ", "ξ", "ψ"];
+// Notes:
+// - αυτή αυτήν requires some extra work
+// - μη and δε are also probably not safe.
+const CANDIDATES_REM_N: &[&str] = &["την", "στην", "Την", "Στην"]; // , "μην", "δεν"];
+const CANDIDATES_ADD_N: &[&str] = &["τη", "στη", "Τη", "Στη"]; // , "μη", "δε"];
 
-// For debugging
-#[allow(dead_code)]
-fn print_context_token(token: &Token, doc: &Doc) {
-    let mut out = Vec::new();
-    for idx in 1..3 {
-        if let Some(ntoken) = doc.get(token.index + idx) {
-            out.push(ntoken.clone());
-        }
-    }
-    println!(
-        "'{}'",
-        out.iter().fold(String::new(), |mut output, token| {
-            let _ = write!(output, "{}{}", token.text, token.whitespace);
-            output
-        })
-    );
-    println!("> '{:?}'\n> '{:?}'", token, out)
+fn remove_last_char(s: &str) -> &str {
+    let mut chars = s.chars();
+    chars.next_back();
+    chars.as_str()
 }
 
 // Care uppercase
 // Passing the token && doc is for dedbug
-#[allow(unused_variables)]
-fn starts_with_vowel_or_plosive(s: &str, token: &Token, doc: &Doc) -> bool {
-    // Manually remove starting punct because our tokenize logic is faulty
-    let ts = s.trim_start_matches(|c: char| !c.is_alphabetic());
-
-    // This only happens if we call this function from a Token that was punct
-    // or if the text is empty, which is an acceptable case
-    if let Some(ch) = ts.chars().next() {
+fn starts_with_vowel_or_plosive(token: &Token) -> bool {
+    debug_assert!(!token.punct);
+    // This only fails if the text is empty, which is an acceptable case
+    if let Some(ch) = token.text.chars().next() {
         PLOSIVE_CLUSTERS
             .iter()
-            .any(|&prefix| ts.to_lowercase().starts_with(prefix))
+            .any(|&prefix| token.text.starts_with(prefix))
             || is_vowel_el(ch)
     } else {
-        // print_context_token(token, doc);
-        // panic!("'{}' was reduced to '{}' at starts_with_vowel", s, ts)
         false
     }
 }
 
-// αυτή αυτήν requires some extra work
-//
-// μη and δε are also probably not safe.
-const FINAL_N_CANDIDATES_WITH: &[&str] = &["την", "στην", "Την", "Στην"]; // , "μην", "δεν"];
-const FINAL_N_CANDIDATES_WITHOUT: &[&str] = &["τη", "στη", "Τη", "Στη"]; // , "μη", "δε"];
+fn get_next_non_punct_token<'a>(token: &'a Token, doc: &'a Doc) -> Option<&'a Token<'a>> {
+    let mut index = token.index + 1;
+    loop {
+        let ntoken = doc.get(index)?;
+        if !ntoken.punct {
+            return Some(ntoken);
+        }
+        index += 1;
+    }
+}
 
 fn remove_final_n_opt(token: &Token, doc: &Doc) -> Option<()> {
-    if FINAL_N_CANDIDATES_WITH.contains(&token.text) {
-        let mut index = 1;
-        loop {
-            let ntoken = doc.get(token.index + index)?;
-            if !ntoken.punct {
-                // First non punct token
-                if !starts_with_vowel_or_plosive(ntoken.text, ntoken, doc) {
-                    return Some(());
-                } else {
-                    return None;
-                }
-            }
-            index += 1
+    if CANDIDATES_REM_N.contains(&token.text) {
+        let ntoken = get_next_non_punct_token(token, doc)?;
+        if !starts_with_vowel_or_plosive(ntoken) {
+            return Some(());
+        } else {
+            return None;
         }
     }
-
     None
 }
 
 pub fn remove_final_n(token: &Token, doc: &Doc, diagnostics: &mut Vec<Diagnostic>) {
     if remove_final_n_opt(token, doc).is_some() {
-        let replacement = format!("{}{}", remove_last_char(token.text), token.whitespace);
-
         diagnostics.push(Diagnostic {
             kind: Rule::RemoveFinalN,
             fix: Some(Fix {
-                replacement,
+                replacement: format!("{}{}", remove_last_char(token.text), token.whitespace),
                 range: token.range,
             }),
         });
@@ -92,36 +72,24 @@ pub fn remove_final_n(token: &Token, doc: &Doc, diagnostics: &mut Vec<Diagnostic
 }
 
 fn add_final_n_opt(token: &Token, doc: &Doc) -> Option<()> {
-    if FINAL_N_CANDIDATES_WITHOUT.contains(&token.text) {
-        let mut index = 1;
-        loop {
-            let ntoken = doc.get(token.index + index)?;
-            if !ntoken.punct {
-                // First non punct token
-                if starts_with_vowel_or_plosive(ntoken.text, token, doc) {
-                    return Some(());
-                } else {
-                    return None;
-                }
-            }
-            index += 1
+    if CANDIDATES_ADD_N.contains(&token.text) {
+        let ntoken = get_next_non_punct_token(token, doc)?;
+        if starts_with_vowel_or_plosive(ntoken) {
+            return Some(());
+        } else {
+            return None;
         }
     }
 
     None
 }
 
-/// Add or remove final ν
 pub fn add_final_n(token: &Token, doc: &Doc, diagnostics: &mut Vec<Diagnostic>) {
     if add_final_n_opt(token, doc).is_some() {
-        let replacement = format!("{}ν{}", token.text, token.whitespace);
-        // let start = token.range.start();
-        // let end = start + replacement.len();
-
         diagnostics.push(Diagnostic {
             kind: Rule::AddFinalN,
             fix: Some(Fix {
-                replacement,
+                replacement: format!("{}ν{}", token.text, token.whitespace),
                 range: token.range,
             }),
         });
@@ -131,9 +99,10 @@ pub fn add_final_n(token: &Token, doc: &Doc, diagnostics: &mut Vec<Diagnostic>) 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::tokenizer::tokenize;
 
     #[test]
-    fn test_remove_basic() {
+    fn test_remove() {
         let text = "θα είναι στην διάθεσή σας";
         let tokens = tokenize(text);
         let mut diagnostics = Vec::new();
@@ -143,5 +112,23 @@ mod tests {
         let mut diagnostics = Vec::new();
         remove_final_n(&tokens[0], &tokens, &mut diagnostics);
         assert!(diagnostics.is_empty());
+    }
+
+    #[test]
+    fn test_add() {
+        let text = "θα είναι στη πόλη σας";
+        let tokens = tokenize(text);
+        let mut diagnostics = Vec::new();
+        add_final_n(&tokens[2], &tokens, &mut diagnostics);
+        assert!(!diagnostics.is_empty());
+    }
+
+    #[test]
+    fn test_next_non_punct_token() {
+        let text = "θα είναι στην, ?διάθεσή σας";
+        let tokens = tokenize(text);
+        let mut diagnostics = Vec::new();
+        remove_final_n(&tokens[2], &tokens, &mut diagnostics);
+        assert!(!diagnostics.is_empty());
     }
 }
