4ac452ba528a61b910f93ef1edd2eae3a96d36cd
daxida <galmaw@hotmail.com>
2025-02-09 16:05:09 +0100

    Fix missing_accent_capital.rs * Now it should not detect words starting with consonants

--- src/rules/missing_accent_capital.rs
+++ src/rules/missing_accent_capital.rs
@@ -1,9 +1,9 @@
 use crate::diagnostic::{Diagnostic, Fix};
 use crate::registry::Rule;
 use crate::tokenizer::{Doc, Token};
-use grac::add_acute_at;
 use grac::has_any_diacritic;
 use grac::syllabify_el;
+use grac::{add_acute_at, is_vowel_el};
 
 /// The first character is uppercase and the rest are lowercase.
 fn is_capitalized(s: &str) -> bool {
@@ -19,10 +19,14 @@ fn is_capitalized(s: &str) -> bool {
 
 // It is frequent to see in newspaper the french rule where
 // the first word of a sentence does not take accent if it should
-// have gone to its first letter.
+// have gone to its first letter (therefore a vowel).
 // Ex. Ηταν μόλις 31…
 fn missing_accent_capital_opt(token: &Token) -> Option<()> {
-    if is_capitalized(token.text) && !has_any_diacritic(token.text) {
+    if is_capitalized(token.text)
+        && !has_any_diacritic(token.text)
+        // We know there is at least one char based on is_capitalized
+        && is_vowel_el(token.text.chars().next().unwrap())
+    {
         Some(())
     } else {
         None
@@ -46,4 +50,27 @@ pub fn missing_accent_capital(token: &Token, _doc: &Doc, diagnostics: &mut Vec<D
             }),
         })
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tokenizer::tokenize;
+
+    macro_rules! test {
+        ($name:ident, $text:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let text = $text;
+                let doc = tokenize(text);
+                let mut diagnostics = Vec::new();
+                missing_accent_capital(&doc[0], &doc, &mut diagnostics);
+                assert_eq!(diagnostics.is_empty(), $expected);
+            }
+        };
+    }
+
+    test!(base_correct, "Άλλο", true);
+    test!(base_wrong, "Αλλο", false);
+    test!(starts_with_consonant, "Χγεννα", true);
 }
