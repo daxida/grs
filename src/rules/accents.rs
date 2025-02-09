2f52245a9bce0dbcbce16c8a8e546a0a902214c6
daxida <galmaw@hotmail.com>
2025-02-09 13:28:18 +0100

    Fix `accents.rs`

--- src/rules/accents.rs
+++ src/rules/accents.rs
@@ -3,7 +3,7 @@ use crate::registry::Rule;
 use crate::tokenizer::{Doc, Token};
 use grac::constants::APOSTROPHES;
 use grac::{
-    add_acute_at, ends_with_diphthong, has_diacritic, remove_diacritic_at, syllabify_el,
+    ends_with_diphthong, has_diacritic, has_diacritics, remove_diacritic_at, syllabify_el,
     syllabify_el_mode, Diacritic, Synizesis,
 };
 
@@ -33,31 +33,32 @@ fn monosyllable_accented_opt(token: &Token, doc: &Doc) -> Option<()> {
 
     // Do not remove accents from abbreviations: όλ' αυτά
     // Nor final periods thay may indicate abbreviation: Μέσ., μέλ.
-    let ntoken = doc.get(token.index + 1)?;
-    if token.whitespace.is_empty() && ntoken.punct {
-        if let Some(npunct_first_char) = ntoken.text.chars().next() {
-            if APOSTROPHES.contains(&npunct_first_char) {
-                return None;
-            }
-            // A final period requires checking that the next word is capitalized
-            if npunct_first_char == '.' {
-                // Consider ellipsis as a black box
-                if ntoken.text.starts_with("...") {
+    if let Some(ntoken) = doc.get(token.index + 1) {
+        if token.whitespace.is_empty() && ntoken.punct {
+            if let Some(npunct_first_char) = ntoken.text.chars().next() {
+                if APOSTROPHES.contains(&npunct_first_char) {
                     return None;
                 }
+                // A final period requires checking that the next word is capitalized
+                if npunct_first_char == '.' {
+                    // Consider ellipsis as a black box
+                    if ntoken.text.starts_with("...") {
+                        return None;
+                    }
 
-                let mut index = 2;
-                loop {
-                    // Should actually return Some(()) if there is no token
-                    let nntoken = doc.get(token.index + index)?;
-                    index += 1;
-                    if !nntoken.punct {
-                        if let Some(nnpunct_first_char) = nntoken.text.chars().next() {
-                            if !nnpunct_first_char.is_uppercase() {
-                                return None;
+                    let mut index = 2;
+                    loop {
+                        // Should actually return Some(()) if there is no token
+                        let nntoken = doc.get(token.index + index)?;
+                        index += 1;
+                        if !nntoken.punct {
+                            if let Some(nnpunct_first_char) = nntoken.text.chars().next() {
+                                if !nnpunct_first_char.is_uppercase() {
+                                    return None;
+                                }
                             }
+                            break;
                         }
-                        break;
                     }
                 }
             }
@@ -87,16 +88,40 @@ pub fn monosyllable_accented(token: &Token, doc: &Doc, diagnostics: &mut Vec<Dia
 }
 
 fn is_multisyllable_not_accented(word: &str) -> bool {
-    syllabify_el(word).len() > 1 && !has_diacritic(word, Diacritic::ACUTE)
+    syllabify_el(word).len() > 1
+        && !has_diacritics(
+            word,
+            &[Diacritic::ACUTE, Diacritic::GRAVE, Diacritic::CIRCUMFLEX],
+        )
 }
+
+// They should never appear on capitalized position, so no uppercase.
+#[rustfmt::skip]
+const CORRECT_MULTISYLLABLE_NOT_ACCENTED: &[&str] = &[
+    "ποτε",
+    // https://el.wiktionary.org/wiki/τινά
+    "τινες", "τινα", "τινε", "τινος", "τινων", "τινοιν", "τινι", "τισι", "τινας",
+    "τονε", "τηνε",
+];
 
 fn multisyllable_not_accented_opt(token: &Token, doc: &Doc) -> Option<()> {
     if !token.greek {
         return None;
     }
 
+    if CORRECT_MULTISYLLABLE_NOT_ACCENTED.contains(&token.text) {
+        return None;
+    }
+
     // Ignore if all caps. Titles do not have accents.
     if token.text.chars().all(|c| c.is_uppercase()) {
+        return None;
+    }
+
+    // Ignore acronyms and some other compounds:
+    // * Α.Υ.
+    // * {{ετικ|λαϊκ|ιατρ}}
+    if token.text.contains(['.', '|', ':']) {
         return None;
     }
 
@@ -129,19 +154,12 @@ fn multisyllable_not_accented_opt(token: &Token, doc: &Doc) -> Option<()> {
 
 pub fn multisyllable_not_accented(token: &Token, doc: &Doc, diagnostics: &mut Vec<Diagnostic>) {
     if multisyllable_not_accented_opt(token, doc).is_some() {
-        // This should have no Fix
-        // but print the error anyway!
-        // tmp solution to print the error: replace it with itself + add random acute
-        let _fix = Fix {
-            replacement: format!("{}{}", add_acute_at(token.text, 1), token.whitespace),
-            range: token.range,
-        };
-        let diagnostic = Diagnostic {
+        // No Fix: we can not know where the accent was supposed to be.
+        diagnostics.push(Diagnostic {
             kind: Rule::MultisyllableNotAccented,
             range: token.range,
             fix: None,
-        };
-        diagnostics.push(diagnostic);
+        });
     }
 }
 
@@ -150,20 +168,30 @@ mod tests {
     use super::*;
     use crate::tokenizer::tokenize;
 
-    macro_rules! test_empty_diagnostics {
-        ($name:ident, $text:expr, $expected:expr) => {
+    macro_rules! test {
+        ($name:ident, $fn:expr, $text:expr, $expected:expr) => {
             #[test]
             fn $name() {
                 let text = $text;
                 let doc = tokenize(text);
                 let mut diagnostics = Vec::new();
-                monosyllable_accented(&doc[0], &doc, &mut diagnostics);
+                $fn(&doc[0], &doc, &mut diagnostics);
                 assert_eq!(diagnostics.is_empty(), $expected);
             }
         };
     }
 
-    test_empty_diagnostics!(test_final_period, "μέλ. Και άλλα.", false);
-    test_empty_diagnostics!(test_abbreviation_period, "μέλ. και άλλα.", true);
-    test_empty_diagnostics!(test_ellipsis, "μέλ... Και άλλα.", true);
+    test!(base_mono_one, monosyllable_accented, "μέλ", false);
+    test!(base_mono_two, monosyllable_accented, "μέλ  ", false);
+    test!(final_period, monosyllable_accented, "μέλ. Και άλλα.", false);
+    test!(
+        abbreviation_period,
+        monosyllable_accented,
+        "μέλ. και άλλα.",
+        true
+    );
+    test!(ellipsis, monosyllable_accented, "μέλ... Και άλλα.", true);
+
+    test!(base_multi, multisyllable_not_accented, "καλημερα", false);
+    test!(acronym, multisyllable_not_accented, "Α.Υ.", true);
 }
