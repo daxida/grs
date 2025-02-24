9bba0d22b6b959accce656f15e7b4f0ea6ee8b28
daxida <galmaw@hotmail.com>
2025-02-25 00:07:37 +0100

    Moved benches to grac

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,5 +1,4 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use grac::{is_greek_word, split_word_punctuation};
 use grs::tokenizer::tokenize;
 use std::fs::File;
 use std::io::{self, Read};
@@ -22,41 +21,6 @@ fn benchmark_tokenize(c: &mut Criterion) {
 
     group.bench_function("tokenize", |b| {
         b.iter(|| tokenize(black_box(&content)));
-    });
-
-    // Word level benches
-    let words: Vec<_> = content
-        .split_inclusive(|c: char| c.is_whitespace())
-        .collect();
-    group.bench_function("split_word_punctuation", |b| {
-        b.iter(|| {
-            for &w in &words {
-                let _ = split_word_punctuation(black_box(w));
-            }
-        });
-    });
-    group.bench_function("is_greek_word", |b| {
-        b.iter(|| {
-            for &w in &words {
-                let _ = is_greek_word(black_box(w));
-            }
-        });
-    });
-
-    // References
-    group.bench_function("ref_split_whitespace_collect", |b| {
-        b.iter(|| {
-            let _: Vec<_> = black_box(content.split_whitespace().collect());
-        });
-    });
-    group.bench_function("ref_split_whitespace_push", |b| {
-        b.iter(|| {
-            let mut words = Vec::new();
-            for word in black_box(content.split_whitespace()) {
-                words.push(word);
-            }
-            black_box(words);
-        });
     });
 }
 
